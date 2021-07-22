105aae5d4ba47de2a127ec52f6dc84395795ad9d
Ben Hopkins <bjhops17@gmail.com>
2021-07-22 06:45:32 -0700

    added base folders

---
+++ routes/api-routes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router()
+
+router.route("/posts")
+.get((req, res)=> {
+
+})
+.post((req, res)=>{
+
+})
+router.post("/user", (req, res)=> {
+
+})
+
+
+module.exports = router
---
+++ routes/html-routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+const path = require("path")
+
+router.get("/profile", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../public/profile.html"))
+})
